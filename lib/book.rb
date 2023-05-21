1c37b123e1cd527f8709d4a32f6d4c7648fc6f84
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-05-21 15:25:00 +0300

    assignment done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+    def initialize(title)
+        @title=title
+        
+    end
+
+    def turn_page
+    puts "Flipping the page...wow, you read fast!" 
+    end
+
+
+    attr_accessor  :page_count, :genre, :title, :author
+
+
+end
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+class Shoe
+    def initialize(brand)
+        @brand=brand
+    end
+
+        attr_accessor :color, :size, :material, :condition, :brand
+    
+
+     def cobble
+        puts 'Your shoe is as good as new!'
+        self.condition='new'
+     
+
+        
+        
+
+
+    end
+end
+
+
+
